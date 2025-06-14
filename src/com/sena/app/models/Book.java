264f6979f81a4f2d1dcf0950fa447779d526d19b
BrayanZarabanda <brayan_zarabanda@soy.sena.edu.co>
2025-06-13 19:12:03 -0500

    Books

--- src/com/sena/app/models/Book.java
+++ src/com/sena/app/models/Book.java
@@ -1,5 +1,118 @@
 package com.sena.app.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Book {
+
+    private static int nextId = 1;
+    private int id;
+    private String title;
+    private String edititionDate;
+    private String editorial;
+    private String isbn;
+
+    private List<String> authors;
+    private boolean readed;
+    private int timeReaded;
+
+
+    public Book(String title, String edititionDate, String editorial, String isbn) {
+        this.id = nextId++;
+        this.title = title;
+        this.edititionDate = edititionDate;
+        this.editorial = editorial;
+        this.isbn = isbn;
+        this.authors = new ArrayList<>();
+        this.readed = false;
+        this.timeReaded = 0;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public static void setNextId(int nextId) {
+        Book.nextId = nextId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEdititionDate() {
+        return edititionDate;
+    }
+
+    public void setEdititionDate(String edititionDate) {
+        this.edititionDate = edititionDate;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public boolean isReaded() {
+        return readed;
+    }
+
+    public void setReaded(boolean readed) {
+        this.readed = readed;
+    }
+
+    public int getTimeReaded() {
+        return timeReaded;
+    }
+
+    public void setTimeReaded(int timeReaded) {
+        this.timeReaded = timeReaded;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", edititionDate='" + edititionDate + '\'' +
+                ", editorial='" + editorial + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                ", readed=" + readed +
+                ", timeReaded=" + timeReaded +
+                '}';
+    }
+
     
 }
